fc259d982b42250d0bf7000620833ea65bf1656d
Atheer <atheerm96@googlemail.com>
2020-09-07 16:21:10 +0100

    add all information to detailscard

--- client/src/Components/DetailsCard.js
+++ client/src/Components/DetailsCard.js
@@ -3,11 +3,18 @@ import { connect } from "react-redux";
 
 
 class DetailsCard extends Component {
+    
     render() {
+
+        const id = this.props.match.params.id;
+
         return (
             <div>
-                <h1>{this.props.habit[this.props.match.params.id].name}</h1>
-            </div >
+                <h1>{this.props.habit[id].name}</h1>
+                <p>{this.props.habit[id].description}</p>
+                <h2>Streak: {this.props.habit[id].streak}</h2>
+                <button>Delete Habit</button>
+            </div>    
         )
     }
 }
