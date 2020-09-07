90fc3d20c258258b64f030049a9229487c1f1072
Jamie Sear <jamiesear@Jamies-MacBook.local>
2020-09-07 11:02:29 +0100

    Made changes

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-think";
+import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import Reducer from "./Reducer/Reducer.js";
 
