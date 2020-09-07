6e50044830940b43b3bd3d1dd0175674e31c23e5
Atheer <atheerm96@googlemail.com>
2020-09-07 12:14:47 +0100

    add seeds for db

--- server/database/dbconfig.js
+++ server/database/dbconfig.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({ database: 'blog' });
+const pool = new Pool({ database: 'l3db' });
 
 function run(query, values, callback) {
     return pool.query(query, values, callback)
--- server/database/seeds.sql
+++ server/database/seeds.sql
@@ -1,14 +1,37 @@
-DROP TABLE IF EXISTS usertable;
+DROP TABLE IF EXISTS users;
 
 
-CREATE TABLE usertable(
+CREATE TABLE users(
     id serial PRIMARY KEY,
-    name_id int not null,
+    name_id int NOT NULL,
     username VARCHAR(20) NOT NULL,
-    user_password VARCHAR(50),
+    user_password VARCHAR(50) NOT NULL
     
 );
 
-INSERT INTO usertable (name_id, username, user_password) VALUES
-    (5, 'Simon', 'enneneneenene');
-   
+INSERT INTO users (name_id, username, user_password) VALUES
+    (1, 'Simon', 'enneneneenene'),
+    (2, 'Atheer', 'hello'),
+    (3, 'Mugisha', 'good'),
+    (4, 'Jamie', 'morning')
+;
+
+
+DROP TABLE IF EXISTS activities;
+
+CREATE TABLE activities(
+    id serial PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    description VARCHAR(500),
+    streak int NOT NULL,
+    latest_date date NOT NULL,
+    name_id int NOT NULL
+);
+
+INSERT INTO activities (name, description, streak, latest_date, name_id) VALUES 
+    ('water','Drink 8 glasses per day', 6, '2020-09-07',2),
+    ('water','Drink 10 glasses per day', 6, '2020-09-05',1),
+    ('sleep','Sleep 8 hours a night', 8, '2020-09-07',2),
+    ('exercise','Exercise 60 mins per day', 0, '2020-09-01',3),
+    ('food','Eat 5 fruits and veg per day', 60, '2020-09-07',4)
+   ; 
