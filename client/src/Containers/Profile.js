44d241f1024507bbeb2560a0f47ed9b2d2abc180
shewitt93 <simon@Simons-iMac.local>
2020-09-10 22:09:02 +0100

    atheer

--- client/src/Containers/Profile.js
+++ client/src/Containers/Profile.js
@@ -7,22 +7,13 @@ class Profile extends Component {
     return (
       <>
         <div className="profileContainer">
-          <h1>{this.props.name}</h1>
-          <h2>Number of Active Habits: {this.props.habits.length}</h2>
-          <h2>
-            Accumulated Current Streak:{" "}
-            {this.props.user.habits.reduce((a, b) => a.streak + b.streak)}
-          </h2>
+          <h1>{this.props.user.name}</h1>
+          <h2>Number of Active Habits: {this.props.user.habits.length}</h2>
         </div>
       </>
     );
   }
 }
 
-<<<<<<< HEAD
-const mSTP = (state) => ({ user: state.user });
+const mSTP = (state) => ({ user: state });
 export default connect(mSTP)(Profile);
-=======
-const mSTP = state => ({user: state})
-export default connect(mSTP)(Profile)
->>>>>>> 0d906c4fb4b1bcede40cd1c28d249ae8537ccb12
