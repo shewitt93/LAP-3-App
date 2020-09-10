bcd717c64ad2a289f696fe66689f7b15c98c7f2c
MugishaU <muwiragiye123@gmail.com>
2020-09-10 21:01:27 +0100

    add redirect for successful register

--- client/src/Containers/NewUser.js
+++ client/src/Containers/NewUser.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import '../styles/newUser.css';
+import "../styles/newUser.css";
 
 export default class NewUser extends Component {
   state = { username: "", email: "", password: "", passwordTwo: "" };
@@ -18,7 +18,6 @@ export default class NewUser extends Component {
         email: this.state.email,
         name: this.state.username,
         password: this.state.password,
-        // password2: this.state.passwordTwo,
       };
       const options = {
         method: "POST",
@@ -27,73 +26,82 @@ export default class NewUser extends Component {
       };
 
       // console.log(options);
-      fetch("http://localhost:3000/users/register", options);
-      // .then((r) => r.json())
-      // .then(addUser)
+      fetch("http://localhost:3000/users/register", options)
+        .then((r) => r.json())
+        .then((data) => {
+          alert(data);
+          if (data.includes("Welcome")) {
+            window.location = `/`;
+          }
+        });
       // .catch(console.warn);
 
       console.log("CONNECT TO DB");
 
       //   console.log(options);
     } else {
-      alert("The passwords do not match, please try again");
+      alert("Passwords do not match");
     }
   };
 
   render() {
     return (
       <>
-      <div className="registrastionPageContainer">
-        <form onSubmit={this.handleFormSubmit}>
-          <input
-            className="formInput"
-            required
-            type="text"
-            name="username"
-            placeholder="Username"
-            value={this.state.username}
-            onChange={this.handleInput}
-          />
-          <br />
-          <input
-            className="formInput"
-            required
-            type="email"
-            name="email"
-            placeholder="Email Address"
-            value={this.state.email}
-            onChange={this.handleInput}
-          />
-          <br />
-          <input
-            className="formInput"
-            required
-            type="password"
-            name="password"
-            minLength="6"
-            placeholder="Password Minimum of 6 characters"
-            value={this.state.password}
-            onChange={this.handleInput}
-          />
-          <br />
-          <input
-            className="formInput"
-            required
-            type="password"
-            name="passwordTwo"
-            minLength="6"
-            placeholder="Re-enter Password"
-            value={this.state.passwordTwo}
-            onChange={this.handleInput}
-          />
-          <br />
-          <input className="registerButton" type="submit" value="Create Account" />
-        </form>
-        <div className="callToAction">
-        <h2>Already have an account?</h2>
-        <br />
-        <Link to="/">Click here to Login</Link>
-        </div>
+        <div className="registrastionPageContainer">
+          <form onSubmit={this.handleFormSubmit}>
+            <input
+              className="formInput"
+              required
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={this.state.username}
+              onChange={this.handleInput}
+            />
+            <br />
+            <input
+              className="formInput"
+              required
+              type="email"
+              name="email"
+              placeholder="Email Address"
+              value={this.state.email}
+              onChange={this.handleInput}
+            />
+            <br />
+            <input
+              className="formInput"
+              required
+              type="password"
+              name="password"
+              minLength="6"
+              placeholder="Password Minimum of 6 characters"
+              value={this.state.password}
+              onChange={this.handleInput}
+            />
+            <br />
+            <input
+              className="formInput"
+              required
+              type="password"
+              name="passwordTwo"
+              minLength="6"
+              placeholder="Re-enter Password"
+              value={this.state.passwordTwo}
+              onChange={this.handleInput}
+            />
+            <br />
+            <input
+              className="registerButton"
+              type="submit"
+              value="Create Account"
+            />
+          </form>
+          <div className="callToAction">
+            <h2>Already have an account?</h2>
+            <br />
+            <Link to="/">Click here to Login</Link>
+          </div>
         </div>
       </>
     );
--- server/routes/users.js
+++ server/routes/users.js
@@ -9,11 +9,11 @@ router.post("/register", emailcheck, async (req, res) => {
   try {
     const { email, name, password } = req.body;
 
-    const user = await pool.query("SELECT * FROM users WHERE email = $1", [
-      email,
+    const user = await pool.query("SELECT * FROM users WHERE (email = $1 or name = $2)", [
+      email, name
     ]);
     if (user.rows.length !== 0) {
-      return res.status(401).json("User already exists");
+      return res.status(401).json("Username or E-mail already registered");
     }
 
     const saltRound = 10;
@@ -21,12 +21,11 @@ router.post("/register", emailcheck, async (req, res) => {
     const hashedWord = await bcrypt.hash(password, salt);
 
     const newUser = await pool.query(
-      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id, password",
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING name",
       [name, email, hashedWord]
     );
 
-    const token = createToken(newUser.rows[0].id);
-    res.json({ token });
+    res.json(`Welcome '${newUser.rows[0].name}', Please log in`);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
